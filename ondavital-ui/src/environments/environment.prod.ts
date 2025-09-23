6184e525d2a18cdbf5b0ec9b4982eaa2c83e5a08
Miguel-GD26 <henrygarcia2616@gmail.com>
2025-09-23 18:06:43 -0500

    first commit

--- ondavital-ui/src/environments/environment.prod.ts
+++ ondavital-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // ¡LA URL DE TU API DESPLEGADA EN RAILWAY!
-  apiUrl: 'https://ritmovital-production-api.up.railway.app/api/classify/'
+  apiUrl: 'https://ritmovital-production.up.railway.app/api/classify/'
 };
